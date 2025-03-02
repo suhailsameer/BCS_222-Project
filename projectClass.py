6803fb6c8fb6766421787a544cf3df24306f30ef
Suhail Sameer <54902370+suhailsameer@users.noreply.github.com>
2025-03-02 11:16:51 +0400

    Initial commit

---
+++ projectClass.py
@@ -0,0 +1,60 @@
+from rply import LexerGenerator
+
+class Lexer:
+    def __init__(self):
+        self.lexer = LexerGenerator()
+    def add_tokens(self):
+        # 1) Keywords
+        # print
+        self.lexer.add('PRINT',r'print')
+        # if
+        self.lexer.add('IF',r'if')
+        # int
+        self.lexer.add('ADD',r'int')
+        # float
+        self.lexer.add('FLOAT',r'float')
+        # string
+        self.lexer.add('STRING',r'string')
+
+        # 2) Arithmetic Operations
+        # Assignment
+        self.lexer.add('ASSIGNMENT',r'=')
+        # Add
+        self.lexer.add('ADD',r'\+')
+        # Subtract
+        self.lexer.add('ADD',r'-')
+        # Multiply
+        self.lexer.add('MULTIPLY',r'*')
+        # Divide
+        self.lexer.add('DIVIDE',r'\/')
+
+        # 3) Conditional Operations
+        # Greater Than (>)
+        self.lexer.add('GREATER',r'>')
+        # Lesser than (<)
+        self.lexer.add('LESSER',r'<')
+        # Equal To (==)
+        self.lexer.add('EQUALTO',r'==')
+        # Not Equal To (!=)
+        self.lexer.add('NOTEQUAL',r'!=')
+
+        # 4} Punctuations
+        # Opening brace
+        self.lexer.add('OPENBRACE',r'\{')
+        # Closing brace
+        self.lexer.add('CLOSINGBRACE',r'\}')
+        # Opening parentheses
+        self.lexer.add('OPENPAREN',r'\(')
+        # Closing parentheses
+        self.lexer.add('CLOSINGPAREN',r'\)')
+        # Semicolon
+        self.lexer.add("ADD",r';')
+
+        # Identifiers
+        self.lexer.add('IDENTIFIERS',r'[a-zA-Z_][a-zA-Z0-9_]*') # Identifiers should start with a letter or underscore and can have numbers, letters, and underscores after
+        # Numbers
+        self.lexer.add('NUMBERS',r'\d+(\.\d+)') # Matches any number including float, does not match if no number present before decimal point (eg, .5)
+
+        # Ignore Whitespaces
+        self.lexer.ignore(r'\s+')
+
